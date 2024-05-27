820e0677e8b5b4db15c6307785dc9eb0eccda821
dechevarrieta <dechevarrieta@vozy.co>
2024-05-27 17:51:25 -0300

    First commit

---
+++ app.jl
@@ -0,0 +1,55 @@
+using Oxygen
+using HTTP
+using JSON
+using StructTypes
+using TextAnalysis
+
+
+
+#CRUD API
+
+#Struct to store conversations
+struct ConversationAnalysis
+    id::Int
+    dialog::String
+    sentiment::String
+    keywords::String
+
+end
+
+#JSON support
+StructTypes.StructType(::Type{ConversationAnalysis}) = StructTypes.Struct()
+
+#Analyze of the conversation
+function analyze_conversation(conversation::String)
+    #TODO Implement conversation analysis
+    return ConversationAnalysis(1, conversation, "Positive", "AI, Julia, Oxygen")
+end
+
+function analyze_dialog(conversation::String)
+    #TODO Implement dialog analysis
+    return ConversationAnalysis(id, conversation, "Positive", "AI, Julia, Oxygen")
+end
+
+function analyze_sentiments(conversation::String)
+    #TODO Implement sentiment analysis
+    return ConversationAnalysis(id, conversation, sentiment, "AI, Julia, Oxygen")
+end
+
+
+#Endpoints
+@post "/api/v1/store-conversation" function (req::HTTP.Request)
+
+    println("Create Conversation")
+    data=JSON.parse(String(HTTP.payload(req)))
+    conversation = data["conversation"]
+    result = analyze_conversation(conversation)
+    println(data)
+    return JSON.json(result)
+    return data
+
+end
+
+serve(PORT=8080)
+
+
