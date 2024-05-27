c5be8221553fcea4823208e0c67507c65a0875f2
elasheras <emperorbfd@gmail.com>
2024-05-27 18:33:55 -0300

    Added automatic dependency import and usage at the entrypoint of the app.

--- app.jl
+++ app.jl
@@ -1,13 +1,23 @@
-using Oxygen
-using HTTP
-using JSON
-using StructTypes
-using TextAnalysis
+# Import Pkg and check for required packages
+import Pkg
 
+# Function to ensure that a package is installed
+function ensure_installed(pkg_name::String)
+    installed = any(dep.name == pkg_name && dep.is_direct_dep for dep in values(Pkg.dependencies()))
+    if !installed
+        Pkg.add(pkg_name)
+    end
+end
+required_packages = ["Oxygen", "HTTP", "JSON", "StructTypes", "TextAnalysis"]
+for pkg in required_packages
+    ensure_installed(pkg)
+end
+
+# Import required packages
+using Oxygen, HTTP, JSON, StructTypes, TextAnalysis
 
 
 #CRUD API
-
 #Struct to store conversations
 struct ConversationAnalysis
     id::Int
@@ -51,5 +61,3 @@ end
 end
 
 serve(PORT=8080)
-
-
